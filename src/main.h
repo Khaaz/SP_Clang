461ac933611422c3942f8069802cc40fae0e180e
Khaazz <khaaz.dev@gmail.com>
2020-02-07 14:15:06 +0100

    init

---
+++ src/main.h
@@ -0,0 +1,14 @@
+typedef enum {false, true} boolean;
+
+typedef struct books {
+    int number;
+    char title[10];
+    boolean isTaken;
+    struct books *next;
+} books_t;
+
+typedef struct categories {
+    char name[3];
+    books_t *books; 
+    struct categories *next; 
+} categories_t;
