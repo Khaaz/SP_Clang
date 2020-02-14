5b5d632acb476c3dc6adab2789d4cd9019deb2cc
Khaazz <khaaz.dev@gmail.com>
2020-02-14 15:11:18 +0100

    formatting

--- src/main.h
+++ src/main.h
@@ -6,17 +6,17 @@ typedef enum
 
 typedef struct books
 {
-    int number;
-    char title[10];
-    boolean isTaken;
-    struct books *next;
+    int            number;
+    char           title[10];
+    boolean        isTaken;
+    struct books * next;
 } books_t;
 
 typedef struct categories
 {
-    char name[3];
-    books_t *books;
-    struct categories *next;
+    char                name[3];
+    books_t *           books;
+    struct categories * next;
 
 } categories_t;
 
@@ -26,10 +26,10 @@ typedef struct borrow
     int date;
 } barrow_t;
 
-categories_t *CreateCategorie(char[3], books_t *);
-books_t *CreateBook(int, char[10]);
-void AddBook(categories_t *, int, char[10]);
-void AddCategorie(categories_t *, char[3], books_t *);
-void AfficheBibli(categories_t *);
-void AddBookWithCategoryName(categories_t *, char[3], int, char[10]);
-void AddFichier(char *);
+categories_t * CreateCategorie(char[3], books_t *);
+books_t *      CreateBook(int, char[10]);
+void           AddBook(categories_t *, int, char[10]);
+void           AddCategorie(categories_t *, char[3], books_t *);
+void           AfficheBibli(categories_t *);
+void           AddBookWithCategoryName(categories_t *, char[3], int, char[10]);
+void           AddFichier(char *);
