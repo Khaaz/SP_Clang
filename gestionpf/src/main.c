24078b58839b8948c70f2aae9d4caf0be39f54dc
MaxBaro18 <maximebarrault18@gmail.com>
2020-03-13 15:18:47 +0100

    reverse fini

--- gestionpf/src/main.c
+++ gestionpf/src/main.c
@@ -6,43 +6,47 @@
 
 
 void reverseStack(stack_t * stack){
-    queue_t *queue = createQueue();
+    queue_t *queue = createQueue(stack->capacity);
     while (!isEmptyStack(stack))
     {
-        pushBack(queue, peek(stack_t));
+        pushBack(queue, peek(stack));
         pop(stack);
     }
     while (!isEmptyQueue(queue))
     {
-        push(stack_t *, front(queue));
+        push(stack, front(queue));
         popFront(queue);
     }
 }
 
 int main()
 {
-    stack_t * stack = createStack(3);
-    push(stack, 5);
-    push(stack, 5);
-    push(stack, 5);
-    pop(stack);
-    pop(stack);
-    pop(stack);
+    stack_t * stack = createStack(4);
+    push(stack, 1);
+    push(stack, 2);
+    push(stack, 3);
+    push(stack, 4);
     displayStack(stack);
-    freeStack(stack);
+    reverseStack(stack);
+    displayStack(stack);
+
+    printf("\nqueue ; \n");
 
     queue_t * queue = createQueue(5);
     pushBack(queue, 1);
     pushBack(queue, 2);
     pushBack(queue, 3);
     pushBack(queue, 4);
-    displayQueue(queue);
+   // displayQueue(queue);
     popFront(queue);
     pushBack(queue, 5);
     pushBack(queue, 6);
     popFront(queue);
     displayQueue(queue);
     freeQueue(queue);
+
+
+
 
     return 0;
 }
