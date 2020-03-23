a8f14050588b897e712ac18e06bff6076f9f7aae
maxbaro18 <maximebarrault18@gmail.com>
2020-03-23 18:53:40 +0100

    Ackermann

--- gestionpf/src/main.c
+++ gestionpf/src/main.c
@@ -1,4 +1,4 @@
-//david.brevet@uca.fr
+// david.brevet@uca.fr
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,23 +6,61 @@
 #include "queue.h"
 #include "stack.h"
 
-
-void reverseStack(stack_t * stack){
-    queue_t *queue = createQueue(stack->capacity);
-    while (!isEmptyStack(stack))
+void reverseStack(stack_t * stack)
+{
+    queue_t * queue = createQueue(stack->capacity);
+    while(!isEmptyStack(stack))
     {
         pushBack(queue, peek(stack));
         pop(stack);
     }
-    while (!isEmptyQueue(queue))
+    while(!isEmptyQueue(queue))
     {
         push(stack, front(queue));
         popFront(queue);
     }
 }
 
+int Ackerman(int m, int n)
+{
+    int       mi     = m;
+    int       ni     = n;
+    stack_t * pile   = createStack(10);
+    int       finBcl = 0;
+    while(finBcl == 0)
+    {
+        if(mi != 0)
+        {
+            while(ni != 0)
+            {
+                push(pile, mi);
+                ni--;
+            }
+            mi--;
+            ni = 1;
+        }
+        else
+        {
+            ni++;
+            if(!isEmptyStack(pile))
+            {
+                mi = peek(pile);
+                pop(pile);
+                mi--;
+            }
+            else
+            {
+                finBcl = 1;
+            }
+        }
+    }
+    return ni;
+    freeStack(pile);
+}
+
 int main()
 {
+
     stack_t * stack = createStack(4);
     push(stack, 1);
     push(stack, 2);
@@ -39,16 +77,13 @@ int main()
     pushBack(queue, 2);
     pushBack(queue, 3);
     pushBack(queue, 4);
-   // displayQueue(queue);
+    // displayQueue(queue);
     popFront(queue);
     pushBack(queue, 5);
     pushBack(queue, 6);
     popFront(queue);
     displayQueue(queue);
     freeQueue(queue);
-
-
-
 
     return 0;
 }
