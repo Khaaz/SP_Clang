6fab70ca4ea4d2b2e957a5e37cb3008a26cb50f2
maxbaro18 <maximebarrault18@gmail.com>
2020-06-05 15:30:37 +0200

    initTable

--- hash/src/main.c
+++ hash/src/main.c
@@ -1,5 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 void initTable(char * path)
-{}
+{
+    char  mot[50];
+    int currentChar = 0, i=0;
+    FILE * file = fopen(path, "r");
+
+    if ( file != NULL)
+    {
+        
+        do
+        {                   
+            while(currentChar != ' ' && currentChar != '.')
+            {
+                currentChar = fgetc(file);
+                mot[i] = currentChar;
+                i++;
+            }
+
+        mot[i-1] = '\0';
+        printf("mot : %s ", mot);
+        currentChar = 0;
+        i=0;
+
+        }while (currentChar != EOF);
+        fclose(file);
+    }
+    else
+    {
+        printf("ERROR");
+    }
+
+}
 
 /**
  * @brief Main
@@ -10,7 +43,7 @@ void initTable(char * path)
  */
 int main(int argc, char ** argv)
 {
-    initTable("./data.text");
+    initTable("test.txt");
 
     return 0;
 }
