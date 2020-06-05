1bae977bc435c7f23ba514e4d93e612eac51feae
Khaazz <khaaz.dev@gmail.com>
2020-06-05 14:46:52 +0200

    init tp hash

---
+++ hash/src/main.c
@@ -0,0 +1,16 @@
+void initTable(char * path)
+{}
+
+/**
+ * @brief Main
+ *
+ * @param argc int
+ * @param argv char**
+ * @return int
+ */
+int main(int argc, char ** argv)
+{
+    initTable("./data.text");
+
+    return 0;
+}
